8282d5f23a78e912ac9ccdf331aead83a8a342c3
Adam Carroll <adam.carroll.2023@mumail.ie>
2025-10-30 17:24:16 +0000

    add NavBar and DataTable components; integrate Bootstrap for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import NavBar from "./Components/NavBar.tsx";
+import DataTable from "./Components/DataTable.tsx";
 
 function App() {
 
   return (
     <>
+        <NavBar></NavBar>
+        <DataTable></DataTable>
     </>
   )
 }
